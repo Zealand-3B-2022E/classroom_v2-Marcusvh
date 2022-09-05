778dcea1e8216005e82ca35b6bffd23eca4b5c78
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-09-05 13:22:29 +0200

    added Studerende class with properties

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Studerende
+    {
+        private string _navn;
+        private int _fødelsmåned;
+        private int _fødelsdag;
+
+        public Studerende(int fødelsmåned, int fødelsdag, string navn)
+        {
+            _navn = navn;          
+            _fødelsmåned = fødelsmåned;
+            _fødelsdag = fødelsdag;
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            private set { _navn = value; }
+        }
+        public int Fødelsmåned
+        {
+            get { return _fødelsmåned; }
+            private set { _fødelsmåned = value; }
+        }
+        
+        public int Fødelsdag
+        {
+            get { return _fødelsdag; }
+            private set { _fødelsdag = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Navn: {Navn}\nFødelsmåned: {Fødelsmåned}\nFødelsdag: {Fødelsdag}";
+        }
+    }
+}
