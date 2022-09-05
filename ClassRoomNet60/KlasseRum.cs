b1cb14875e2c0eda02060ba21919a2f5ea2b4223
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-09-05 13:29:14 +0200

    added class klasserum with defaut const and auto prop

---
+++ ClassRoomNet60/KlasseRum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class KlasseRum
+    {
+        private string _klasseNavn;
+        private List<Studerende> _klasseliste;
+        private DateTime _semesterStart;
+
+        public KlasseRum() { }
+
+        public string KlasseNavn { get; set; }
+        public List<Studerende> Klasseliste { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public override string ToString()
+        {
+            return $"Klasse navn: {KlasseNavn}\nKlasse liste: {Klasseliste.Count}\nSemester start: {SemesterStart}";
+        }
+    }
+}
