ca52b0d7d3782dd60032bb574156fec6cf6e4665
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-09-05 15:27:25 +0200

    added hvor mange der har fødelsdag i hvilke måneder. added fødselsmåned validering. men skal flyttes.

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -2,11 +2,12 @@
 using ClassRoomNet60;
 
 Console.WriteLine("Hello, World!");
-
+Service service = new Service();
 
 List<Studerende> studerendeList = new List<Studerende>();
 studerendeList.Add(new Studerende("Marcus", 10, 19));
 studerendeList.Add(new Studerende("Cecilie", 3, 31));
+studerendeList.Add(new Studerende("welpson", 14, 31));
 
 KlasseRum klasseRum1 = new KlasseRum();
 klasseRum1.KlasseNavn = "3B";
@@ -15,12 +16,22 @@ klasseRum1.SemesterStart = DateTime.Parse("01-09-2022");
 
 
 
-Console.WriteLine(klasseRum1.KlasseNavn);
-foreach (Studerende stud in klasseRum1.Klasseliste)
-{
-    Console.WriteLine(stud);
-}
-Console.WriteLine(klasseRum1.SemesterStart);
+//Console.WriteLine(klasseRum1.KlasseNavn);
+//foreach (Studerende stud in klasseRum1.Klasseliste)
+//{
+//    Console.WriteLine(stud);
+//}
+//Console.WriteLine(klasseRum1.SemesterStart);
 
-Service service = new Service();
-Console.WriteLine(service.Årstid(klasseRum1.Klasseliste.Find(k => k.Navn == "Cecilie").Fødelsmåned)); 
+//Console.WriteLine(service.Årstid(klasseRum1.Klasseliste.Find(k => k.Navn == "Cecilie").Fødelsmåned)); 
+
+
+//try
+//{
+//    service.HvilkenÅrstid(klasseRum1);
+//}
+//catch (ArgumentOutOfRangeException e)
+//{
+//    Console.WriteLine(e.ParamName);
+//}
+service.HvilkenÅrstid(klasseRum1);
--- ClassRoomNet60/Service.cs
+++ ClassRoomNet60/Service.cs
@@ -31,5 +31,46 @@ namespace ClassRoomNet60
 
             return årstid;
         }
+
+        public void HvilkenÅrstid(KlasseRum klasseRum)
+        {
+            int vinter = 0;
+            int forår = 0;
+            int sommer = 0;
+            int efterår = 0;
+
+            foreach (var list in klasseRum.Klasseliste)
+            {
+                int måned = list.Fødelsmåned;
+                if(måned <= 0 || måned > 12)
+                {
+                    //throw new ArgumentOutOfRangeException("welp");
+                    return;
+                }
+
+                if (måned == 12 || måned == 1 || måned == 2)
+                {
+                    vinter++;
+                }
+                if (måned == 3 || måned == 4 || måned == 5)
+                {
+                    forår++;
+                }
+                if (måned == 6 || måned == 7 || måned == 8)
+                {
+                    sommer++;
+                }
+                if (måned == 9 || måned == 10 || måned == 11)
+                {
+                    efterår++;
+                }
+            }
+
+            Console.WriteLine($"Vinter: {vinter}");
+            Console.WriteLine($"Forår: {forår}");
+            Console.WriteLine($"Sommer: {sommer}");
+            Console.WriteLine($"Efterår: {efterår}");
+        }
     }
+
 }
